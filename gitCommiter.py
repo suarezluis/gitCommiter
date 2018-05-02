def20e55315669f7b6ca574d216f6d1183259317
suarezlui <suarezluis@gmail.com>
2018-05-02 13:13:08 -0500

    "commiter auto"

--- gitCommiter.py
+++ gitCommiter.py
@@ -1,6 +1,6 @@
 from subprocess import call
 
-commits = 2
+commits = 20
 filename = "gitter.txt"
 pusher = "git pull https://suarezluis:makemoney2903@github.com/suarezluis/gitCommiter/ master"
 for i in range(1,commits):
