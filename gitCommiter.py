5e646295870f4299cd8cf897f172532a08520c0f
suarezlui <suarezluis@gmail.com>
2018-05-02 13:10:14 -0500

    "commiter auto"

--- gitCommiter.py
+++ gitCommiter.py
@@ -2,11 +2,12 @@ from subprocess import call
 
 commits = 2
 filename = "gitter.txt"
+pusher = "git pull https://suarezluis:makemoney2903@github.com/suarezluis/gitCommiter/ master"
 for i in range(1,commits):
     f= open(filename,"w+")
     f.close()
     call(["git", "add", "."])
     call(["git", "commit", "-m",'"commiter auto"'])
     call(["git", "push"])
-    call(["makemoney"])
+    call(pusher)
     
