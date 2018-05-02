d7febf80e58d1810f44fbb473e5f7b5fd5ea973e
suarezlui <suarezluis@gmail.com>
2018-05-02 13:20:35 -0500

    asd

--- gitCommiter.py
+++ gitCommiter.py
@@ -2,7 +2,7 @@ from subprocess import call
 
 commits = 2
 filename = "gitter.txt"
-pusher = "git pull https://suarezluis:makemoney2903@github.com/suarezluis/gitCommiter/ master"
+pusher = "git push https://suarezluis:makemoney2903@github.com/suarezluis/gitCommiter/ master"
 for i in range(1,commits):
     f= open(filename,"w+")
     f.close()
