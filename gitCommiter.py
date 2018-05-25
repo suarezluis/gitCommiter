bc7c9dfad0c1c2607a283006c041f371009e5746
suarezluis <suarezluis@gmail.com>
2018-05-25 03:27:45 -0500

    improved algorith

--- gitCommiter.py
+++ gitCommiter.py
@@ -11,25 +11,27 @@ commited = 0
 def git():
     call(["clear"])
     call(["git", "add", "."])
+    print("Programmed by Luis Suarez May 2018")
+    print("================================================================")
     print("Added all files")
     print("================================================================")
     call(["git", "commit", "-m",'"commiter auto"']) 
     print("================================================================")
-    print("Git commited! pushed " + str(commited) + " / " + str(commits))
+    print("Git commited! " + str(commited) + " / " + str(commits))
     print("================================================================")
     print("Pushing...")
     print("================================================================")
     ##call(["git", "push", pusher])
     print("================================================================")
-    print("Pushed!")
-    print("Waiting 3 seconds")
+    print("Commited!")
+    print("Waiting 1 seconds")
     print("================================================================")
-    sleep(1)
+    sleep(0)
 
 
 
 
-for i in range(1,commits+1):
+for i in range(1,commits):
     commited = i
     call(["rm", "*.me"])
     file = filename + str(i) + extention
